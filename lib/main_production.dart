3928ffcd197073faf6a5ac4419f7197e76bc52c2
mbugua <kelvin.mbugua@riverbank.co.ke>
2025-06-16 18:18:12 +0300

    Remove counter tests and update app configuration setup
    Deleted CounterCubit and CounterView tests as they are no longer needed. Adjusted import order and added flavor-specific setup in app configuration for better environment management. Cleaned up unused comments in bootstrap file.

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,6 +1,8 @@
 import 'package:zed_nano/app/app.dart';
+import 'package:zed_nano/app/app_config.dart';
 import 'package:zed_nano/bootstrap.dart';
 
 void main() {
+  AppConfig.setFlavor(Flavor.production);
   bootstrap(() => const App());
 }
