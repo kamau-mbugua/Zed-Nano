3f7ae49b3a01d48049b8423344a3ad82fbbc45db
mbugua <kelvin.mbugua@riverbank.co.ke>
2025-06-23 10:15:24 +0300

    Refactor password to PIN and add ResetPinScreen
    Replaced all instances of "Password" with "PIN" for better terminology consistency. Added a new `ResetPinScreen` to handle PIN reset functionality, allowing users to reset their PIN using email or phone number.

---
+++ lib/screens/auth/set_new_pin_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SetNewPinPage extends StatefulWidget {
+  const SetNewPinPage({Key? key}) : super(key: key);
+
+  @override
+  _SetNewPinPageState createState() => _SetNewPinPageState();
+}
+
+class _SetNewPinPageState extends State<SetNewPinPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
