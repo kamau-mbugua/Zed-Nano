9f2b3dc166e44680f9f2c9eb485c872639df2bb9
mbugua <kelvin.mbugua@riverbank.co.ke>
2025-06-24 19:38:45 +0300

    Add skeleton pages for business creation and subscription flow
    Created four placeholder pages: `CompletedBusinessCreationPage`, `ChooseSubscriptionPage`, `BusinessCreatedPreviewPage`, and `CreateBusinessPage`. These pages currently contain basic structures and will be further implemented in the next steps.

---
+++ lib/screens/business/create_business/business_created_preview_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BusinessCreatedPreviewPage extends StatelessWidget {
+  const BusinessCreatedPreviewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/business/create_business/completed_business_creation_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompletedBusinessCreationPage extends StatelessWidget {
+  const CompletedBusinessCreationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
