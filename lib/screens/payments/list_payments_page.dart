329098f5bb92aec879d5b6e713e59d74bea053df
mbugua <kelvin.mbugua@riverbank.co.ke>
2025-06-24 19:13:13 +0300

    Add placeholder pages for payments, products, and categories

---
+++ lib/screens/payments/list_payments_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ListPaymentsPage extends StatelessWidget {
+  const ListPaymentsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
